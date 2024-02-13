fe81eeab287b8ca325b11ff6b2b29d29653cc7df
Mitrich-rus <146307143+Mitrich-rus@users.noreply.github.com>
2024-02-13 17:22:20 +1000

    HW done!

--- final_homework.py
+++ final_homework.py
@@ -11,5 +11,5 @@ data.head()
 heads = data['whoAmI'].unique()
 one_hot_view = pd.DataFrame()
 for el in heads:
-    one_hot_view[el] = data['whoAmI'] == el
-print(one_hot_view.head())
+    one_hot_view[el] = (data['whoAmI'] == el).astype(int)
+print(one_hot_view)
